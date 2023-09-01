b160fe726054c85ec74fa64c3bc6c532c8d453ef
Bianca <dev3@labsystem.com.br>
2023-09-01 14:55:01 -0300

    att

--- Arla32/Controllers/HomeController.cs
+++ Arla32/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace Arla32.Controllers
         {
             return View();
         }
+        public IActionResult Anexod()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
