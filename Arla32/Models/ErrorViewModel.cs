533cd41bff1a5982e0c844b3cc14b8dc652b39e7
LabsystemDev1 <137182284+LabsystemDev1@users.noreply.github.com>
2023-09-01 12:01:17 -0300

    First Deploy

---
+++ Arla32/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Arla32.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
