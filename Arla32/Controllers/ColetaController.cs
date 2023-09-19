154f4d19805b70ab2158664e96705747bd6cfcfc
Bianca <dev3@labsystem.com.br>
2023-09-19 08:01:13 -0300

    att

--- Arla32/Controllers/ColetaController.cs
+++ Arla32/Controllers/ColetaController.cs
@@ -47,5 +47,22 @@ namespace Arla32.Controllers
             ViewBag.OS = OS;
             return View();
         }
+
+
+        [HttpPost]
+
+        public async Task<IActionResult> SalvarAldeidos (int OS, [Bind("data_ini,data_term,os,col_norma,col_np,col_desc,lote_sol," +
+            "codigo_curva,fator_calibracao,massa_branco,absorbancia_branco,maximo_permitido,massa_amostra,absorbancia_amostra\"" +
+            " carta_absorbancia,carta_concentracao,mat_prima1,mat_lote1,mat_validade,mat_prima3,mat_lote3,mat_validade3,mat_prima4,mat_lote4,mat_validade4," +
+            "inst_desc1,inst_validade1,inst_desc2,inst_cod2,inst_validade2,inst_desc1_1,inst_cod1_1,inst_validade1_1,inst_desc2_2,inst_cod_2_2,inst_validade2_2 "
+              )] ColetaModel.Aldeidos aldeidos ){
+
+        }
+
+           
+
+
+
+
     }
 }
